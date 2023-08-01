545e3c8965eb56377bf4472c3694f2e3a4e36859
Sophie <nguyensophie721@gmail.com>
2023-08-01 11:39:05 -0400

    Initial Commit

---
+++ vitalmentor/vitalmentorApp.swift
@@ -0,0 +1,17 @@
+//
+//  vitalmentorApp.swift
+//  vitalmentor
+//
+//  Created by scholar on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct vitalmentorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
